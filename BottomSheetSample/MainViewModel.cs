33a1e3228cc5bd49d6c43d88532f1381f5f7bdb8
John Doherty <jdoherty@esri.com>
2025-03-19 11:55:59 -0600

    Sample for issue testing

--- BottomSheetSample/MainViewModel.cs
+++ BottomSheetSample/MainViewModel.cs
@@ -17,7 +17,6 @@ public partial class MainViewModel
     {
         this.bottomSheetNavigationService = bottomSheetNavigationService;
 
-        // Register to receive StartAddRouteModeRequestMessage message
         WeakReferenceMessenger.Default.Register<OpenBottomSheetMessage>(this, async (r, m) =>
         {
             await OpenBottomSheet2();
